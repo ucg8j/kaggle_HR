970195d82267c6f8f72f3a429560f11772aba2ba
ucg8j <lukesingham@gmail.com>
2017-01-05 13:52:06 +1100

    check for missing vals and add pairwise plots

--- model.py
+++ model.py
@@ -2,16 +2,17 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import os
+from random import sample
 
 #### set working dir and read data
-path = os.path.expanduse('~/Projects/kaggle_HR/')
+path = os.path.expanduser('~/Projects/kaggle_HR/')
 os.chdir(path)
 df = pd.read_csv('HR_comma_sep.csv')
 
 
 #### explore the data set
 col_names = df.columns.tolist()
-df.shape # (14999, 11)
+df.shape # (14999, 10)
 df.describe
 
 # some basic qtys
@@ -22,14 +23,20 @@ print '# people stayed = {}'.format(len(df[df['left'] == 0]))
 print '% people stayed = {}%'.format(round(float(len(df[df['left'] == 1])) / len(df) * 100), 3)
 # % people stayed = 24.0%
 
+# check missing for values
+df.apply(lambda x: sum(x.isnull()), axis=0)
+# no missing values
+
 # correlation heatmap
 correlation = df.corr()
 plt.figure(figsize = (10,10))
 sns.heatmap(correlation, vmax=1, square=True, annot=True, cmap='cubehelix')
-plt.title('Correlation between numeric features')
 
-# TODO fix this plot
-#pplot = sns.pairplot(df)
+# pairwise plots
+df_sampl = df.sample(frac=0.05)
+pplot = sns.pairplot(df_sampl, hue="left")
+pplot_v2 = sns.pairplot(df_sampl, diag_kind="kde")
+
 
 #### feature eng / expand possible variables
 df['productivity'] = df.average_montly_hours / df.number_projectdf
