e5ebfab4ab24b733df1f55396b805ea516d856d8
ucg8j <lukesingham@gmail.com>
2017-01-05 11:39:48 +1100

    corrected exploratory analysis, changed function from .size to len()

--- model.py
+++ model.py
@@ -12,10 +12,10 @@ columns_names = df.columns.tolist()
 df.shape # (14999, 11)
 
 
-print('# people left = {}'.format(df[df['left']==1].size))
-# people left = 39281
-print('# people stayed = {}'.format(df[df['left']==0].size))
-# of people stayed = 125708
+print('# people left = {}'.format(len(df[df['left']==1])))
+# people left = 3571
+print('# people stayed = {}'.format(len(df[df['left']==0])))
+# people stayed = 11428
 print('% people stayed = {}%'.format(round(float(len(df[df['left']==1])) / len(df) * 100),3))
 # % people stayed = 24.0%
 
